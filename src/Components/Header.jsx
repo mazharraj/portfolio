885e6b79cc6bcb511ea3a2a021742f1846137b14
SAYMON AREFIN AKASH <75067011+saymonakash@users.noreply.github.com>
2023-07-21 18:41:21 +0600

    Add files via upload

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   let [open, setOpen] = useState(false);
   return (
     <div>
-      <div className="flex lg:flex-row flex-col px-6 lg:px-20 justify-between lg:items-center bg-gray-900 py-3 shadow-lg">
+      <div className="flex justify-between px-6 lg:px-20 justify-between items-center bg-gray-900 py-3 shadow-lg relative">
         <div className="flex items-center gap-2">
           <a href="#">
             <img src="vite.svg" alt="" className="logo" />
@@ -17,47 +17,45 @@ function Header() {
 
         <div
           onClick={() => setOpen(!open)}
-          className="absolute right-6 text-4xl text-white lg:hidden cursor-pointer">
+          className="text-4xl text-white lg:hidden cursor-pointer">
           <ion-icon name={open ? "close-outline" : "menu-outline"}></ion-icon>
         </div>
-
-        <ul
-          className={`absolute lg:static lg:bg-gray-900 bg-gray-700 w-[70%] flex right-0 top-19 lg:flex-row flex-col gap-6 text-white z-[-1] lg:z-auto h-[90%] justify-center lg:justify-end items-center text-xl lg:text-lg duration-500 ease-in ${
-            open ? "top-0" : "top-[-600px]"
-          }`}>
-          <li>
-            <a href="#" className="hover:text-violet-200 duration-300">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-violet-200 duration-300">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-violet-200 duration-300">
-              Service
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-violet-200 duration-300">
-              Skill
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-violet-200 duration-300">
-              Gallery
-            </a>
-          </li>
-
-          <li>
-            <a href={"cv.pdf"} download>
-              <Button btn="Contact Now" />
-            </a>
-          </li>
-        </ul>
       </div>
+      <ul
+        className={`absolute lg:static lg:bg-gray-900 bg-gray-700 w-full top-full flex right-0 top-0 lg:flex-row flex-col gap-6 text-white lg:z-auto justify-center lg:justify-end items-center text-xl lg:text-lg duration-500 overflow-hidden ${
+          open ? "h-screen" : "h-0"
+        }`}>
+        <li>
+          <a href="#" className="hover:text-violet-200 duration-300">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-violet-200 duration-300">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-violet-200 duration-300">
+            Service
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-violet-200 duration-300">
+            Skill
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-violet-200 duration-300">
+            Gallery
+          </a>
+        </li>
+        <li>
+          <a href={"cv.pdf"} download>
+            <Button btn="Contact Now" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
